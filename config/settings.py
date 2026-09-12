552300b377bd748a0096c98179adc6be19e4a83b
JulsMonjaraz <julary2003@gmail.com>
2026-09-12 14:38:11 -0600

    Añadir logging para depurar error 500

--- config/settings.py
+++ config/settings.py
@@ -212,3 +212,24 @@ SOCIALACCOUNT_LOGIN_ON_GET = True
 
 # Google Maps
 GOOGLE_MAPS_API_KEY = os.environ.get('GOOGLE_MAPS_API_KEY', '')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'ERROR',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    },
+}
